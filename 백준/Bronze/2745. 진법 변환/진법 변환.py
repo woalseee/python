0153af1d5d110d49c9456c0f2696c2289336fe3b
jm <109592496+woalseee@users.noreply.github.com>
2025-07-21 15:29:51 +0900

    [Bronze II] Title: 진법 변환, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,2 +1,12 @@
 n,b = input().split()
-print(int(n,int(b)))
+n = list(n)
+n=n[::-1]
+b = int(b)
+
+num="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+cnt = 0
+
+for i in range(len(n)):
+    cnt = cnt + (num.index(n[i])*(b**i))
+
+print(cnt)
