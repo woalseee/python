37df7f6a6b9fa239018b62857bef6b799686e959
jm <109592496+woalseee@users.noreply.github.com>
2025-07-21 15:19:50 +0900

    [Bronze II] Title: 진법 변환, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,9 +1,2 @@
-N, B = input().split()
-alpabet_list = list("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-N = N[::-1]
-result = 0 
-
-for i,n in enumerate(N):
-    result += alpabet_list.index(n)* int(B) ** int(i)
-    
-print(result)
+n,b = input().split()
+print(int(n,int(b)))
