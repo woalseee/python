95234308fedc6c418b40d6cc6585b1d144f5436a
jm <109592496+woalseee@users.noreply.github.com>
2025-07-23 08:39:46 +0900

    [Bronze I] Title: 약수들의 합, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -0,0 +1,15 @@
+while True:
+    n = int(input())
+    if n == -1:
+        break
+    
+    arr = []
+    for i in range(1,n):
+        if n%i==0:
+            arr.append(i)
+    
+    if sum(arr) == n:
+        print(n,"=",end=" ")
+        print(*arr,sep=" + ") # sep 활용을 잘하자
+    else:
+        print("%d is NOT perfect." %n)      
